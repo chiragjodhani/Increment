3c30d3ad12250e7284b1335776933a58dd0c0d49
Chirag Jodhani <chatur.iosdeveloper@gmail.com>
2020-09-21 11:19:07 +0530

    Flexible GridItems UI With Retry & Fixing Leak | SwiftUI MVVM iOS App: Part 16 | iOS 14 | Xcode 12

--- Increment/ChallengeListView.swift
+++ Increment/ChallengeListView.swift
@@ -10,15 +10,34 @@ import SwiftUI
 struct ChallengeListView: View {
     @StateObject private var viewModel = ChallengeListViewModel()
     var body: some View {
+        ZStack {
+            if viewModel.isLoading  {
+                ProgressView()
+            }else if let error = viewModel.error {
+                VStack(spacing: 10) {
+                    Text(error.localizedDescription)
+                    Button(action: {
+                        self.viewModel.send(action: .retry)
+                    }) {
+                        Text("Retry")
+                    }.padding(10).background(Rectangle().fill(Color.red).cornerRadius(5))
+                }
+            }else {
+                mainContentView
+            }
+        }
+    }
+    
+    var mainContentView: some View {
         ScrollView {
             VStack {
-                LazyVGrid(columns: [.init(.flexible()), .init(.flexible())]) {
+                LazyVGrid(columns: [.init(.flexible(), spacing: 20), .init(.flexible())], spacing: 20) {
                     ForEach(self.viewModel.itemsViewModels, id: \.self) { viewmodel in
                         ChallengeItemView(viewmodel)
                     }
                 }
                 Spacer()
-            }
+            }.padding(10)
         }.navigationTitle(viewModel.title)
     }
 }
@@ -28,16 +47,33 @@ struct ChallengeItemView: View {
     init(_ viewModel: ChallengeItemViewModel) {
         self.viewModel = viewModel
     }
+    
+    var titleRow: some View {
+        HStack {
+        Text(viewModel.title)
+            .font(.system(size: 24, weight: .bold))
+            Spacer()
+            Image(systemName: "trash")
+        }
+    }
+    
+    var dailyIncreaseRow: some View
+    {
+        HStack {
+            Text(viewModel.dailyIncreaseText)
+                .font(.system(size: 24, weight: .bold))
+            Spacer()
+        }
+    }
     var body: some View {
         HStack {
             Spacer()
             VStack {
-                Text(viewModel.title)
-                    .font(.system(size: 24, weight: .bold))
-                Text(viewModel.statusText)
-                Text(viewModel.dailyIncreaseText)
-            }.padding()
+                titleRow
+                Text(viewModel.statusText).font(.system(size: 12, weight: .bold)).padding(25)
+                dailyIncreaseRow
+            }.padding(.vertical, 10)
             Spacer()
-        }.background(Rectangle().fill(Color.darkPrimaryButton).cornerRadius(5)).padding()
+        }.background(Rectangle().fill(Color.primaryButton).cornerRadius(5))
     }
 }
--- Increment/ViewModels/ChallengeListViewModel.swift
+++ Increment/ViewModels/ChallengeListViewModel.swift
@@ -12,29 +12,48 @@ final class ChallengeListViewModel: ObservableObject {
     private let challengeService: ChallengeServiceProtocol
     private var cancellable: [AnyCancellable] = []
     @Published private(set) var itemsViewModels: [ChallengeItemViewModel] = []
-    
+    @Published private(set) var error: IncrementError?
+    @Published private(set) var isLoading = false
     let title = "Challenges"
+    enum Action {
+        case retry
+    }
+    
     init(userService: UserServiceProtocol = UserService(),
          challengeService: ChallengeServiceProtocol = ChallengeService()) {
         self.userService = userService
         self.challengeService = challengeService
         observeChallenges()
     }
-    
+    func send(action: Action) {
+        switch action {
+        case .retry:
+            observeChallenges()
+        }
+    }
     private func observeChallenges() {
+        isLoading = true
         userService.currentUser().compactMap {
             $0?.uid
-        }.flatMap { userId -> AnyPublisher<[Challenge], IncrementError> in
+        }.flatMap { [weak self] userId -> AnyPublisher<[Challenge], IncrementError> in
+            guard let self = self else { return Fail(error: .default()).eraseToAnyPublisher()}
             return self.challengeService.observeChallenges(userId: userId)
-        }.sink { (completion) in
+        }.sink { [weak self] completion in
+            guard let self = self else { return}
+            self.isLoading = false
             switch completion  {
             case let .failure(error):
-                print(error.localizedDescription)
+                self.error = error
             case .finished:
                 print("Finished")
             }
-        } receiveValue: { challenges in
+        } receiveValue: { [weak self] challenges in
+            guard let self = self else { return}
+            self.isLoading = false
+            self.error = nil
             self.itemsViewModels = challenges.map{.init($0)}
         }.store(in: &cancellable)
     }
+    
+    
 }
